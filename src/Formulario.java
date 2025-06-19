e56d60bb6f73d72618e77318e77f61cf92fdf8f7
POO <emily.galeas@epn.edu.ec>
2025-06-19 09:55:18 -0500

    boton Mostrar en Formulario

--- src/Formulario.java
+++ src/Formulario.java
@@ -12,11 +12,11 @@ public class Formulario extends JFrame {
     private JTextField estatura;
     private JButton BUTTONmostrarEnFormulario;
     private JButton BUTTONmostrarDatos;
-    private JLabel nombreText;
+    private JLabel datosFormulario;
 
     public Formulario(){
         setTitle("FORMULARIO DATOS PERSONALES");
-        setSize(800,500);
+        setSize(600,450);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setContentPane(Principal);
         setVisible(true);
@@ -33,10 +33,14 @@ public class Formulario extends JFrame {
         BUTTONmostrarEnFormulario.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e){
-                String
+                String datos = obtenerDatos();
+                String datos2 = "<html>" + datos.replace("\n","<br>") + "<html>";
+                datosFormulario.setText(datos2);
             }
         });
+
     }
+    // "<html> <br> Nombre: <br> Apellido: <br> Dirección: <br> Teléfono: <br> Año de nacimiento: <br> Estatura:     metros <html>"
 
     private String obtenerDatos() {
         return "Nombre: " + nombre.getText() + "\n" + "Apellido: " + apellido.getText() + "\n" + "Dirección: " + direccion.getText() + "\n" +
